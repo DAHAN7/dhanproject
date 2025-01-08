6c902c5707419d04929dc39c2c2160aa36c3f902
DAHAN7 <162085296+DAHAN7@users.noreply.github.com>
2025-01-08 21:51:38 +0900

    2025-01-08

--- dahan/src/main/java/com/shop/repository/ItemRepository.java
+++ dahan/src/main/java/com/shop/repository/ItemRepository.java
@@ -1,33 +1,27 @@
 package com.shop.repository;
 
-import java.util.List;
-
+import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
-import com.shop.entity.Item;
+import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
-	List<Item> findByItemNm(String itemNm);
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom{
 
-	// 상품명을 기준으로 상세 설명도 함께 검색 (AND 조건)
-	List<Item> findByItemNmAndItemDetail(String itemNm, String itemDetail);
+    List<Item> findByItemNm(String itemNm);
 
-	// 상품명을 기준으로 상세 설명을 포함한 검색 (OR 조건)
-	List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
-	// 특정 가격 이하 상품 조회
-	List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThan(Integer price);
 
-	// 특정 가격 이하 상품을 가격 내림차순으로 조회
-	List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
-	// JPQL을 사용한 상세 설명 검색 (내림차순 정렬)
-	@Query("SELECT i FROM Item i WHERE i.itemDetail LIKE %:itemDetail% ORDER BY i.price DESC")
-	List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
-	// 네이티브 SQL을 사용한 상세 설명 검색 (내림차순 정렬)
-	@Query(value = "SELECT * FROM item i WHERE i.item_detail LIKE %:itemDetail% ORDER BY i.price DESC", nativeQuery = true)
-	List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
 }
